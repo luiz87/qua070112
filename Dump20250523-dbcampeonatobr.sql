9f76c3ff8714fbbbcdb45bd671488b214c9b5272
Luiz Felipe <luizfelipe.freire@gmail.com>
2025-05-27 19:33:04 -0300

    Update Dump20250523-dbcampeonatobr.sql

--- Dump20250523-dbcampeonatobr.sql
+++ Dump20250523-dbcampeonatobr.sql
@@ -1,3 +1,6 @@
+DROP DATABASE IF EXISTS dbcampeonatobr;
+CREATE DATABASE dbcampeonatobr;
+USE dbcampeonatobr;
 -- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: dbcampeonatobr
